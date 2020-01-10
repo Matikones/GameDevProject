6691c96e179c8264be9488c23146c5f67dd99077
Matikones <matiadamek97@gmail.com>
2020-01-11 00:28:11 +0100

    Update

---
+++ Prototype 1/Assets/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Text city;
+    public static bool camp = false;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("City1");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
+        camp = true;
+    }
+
+    public void FreeRoam1()
+    {
+        camp = false;
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("FreeRoam1", LoadSceneMode.Additive);
+    }
+    public void FreeRoam2()
+    {
+        camp = false;
+        SceneManager.LoadScene("City2");
+        SceneManager.LoadScene("FreeRoam2", LoadSceneMode.Additive);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (MainMenu.camp == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (GameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        GameIsPaused = false;
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+    public void Next1()
+    {
+        GameIsPaused = false;
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level2", LoadSceneMode.Additive);
+    }
+    public void Next2()
+    {
+        GameIsPaused = false;
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level3", LoadSceneMode.Additive);
+    }
+    public void Next3()
+    {
+        GameIsPaused = false;
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level4", LoadSceneMode.Additive);
+    }
+    public void Next4()
+    {
+        GameIsPaused = false;
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level5", LoadSceneMode.Additive);
+    }
+    public void Next5()
+    {
+        GameIsPaused = false;
+        SceneManager.LoadScene("City2");
+        SceneManager.LoadScene("Level6", LoadSceneMode.Additive);
+    }
+    public void Next6()
+    {
+        GameIsPaused = false;
+        SceneManager.LoadScene("City2");
+        SceneManager.LoadScene("Level7", LoadSceneMode.Additive);
+    }
+    public void Next7()
+    {
+        GameIsPaused = false;
+        SceneManager.LoadScene("City2");
+        SceneManager.LoadScene("Level8", LoadSceneMode.Additive);
+    }
+    public void Next8()
+    {
+        GameIsPaused = false;
+        SceneManager.LoadScene("Finish");
+    }
+
+    public void Restart1()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Restart");
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
+    }
+    public void Restart2()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Restart");
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level2", LoadSceneMode.Additive);
+    }
+    public void Restart3()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Restart");
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level3", LoadSceneMode.Additive);
+    }
+    public void Restart4()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Restart");
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level4", LoadSceneMode.Additive);
+    }
+    public void Restart5()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Restart");
+        SceneManager.LoadScene("City1");
+        SceneManager.LoadScene("Level5", LoadSceneMode.Additive);
+    }
+    public void Restart6()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Restart");
+        SceneManager.LoadScene("City2");
+        SceneManager.LoadScene("Level6", LoadSceneMode.Additive);
+    }
+    public void Restart7()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Restart");
+        SceneManager.LoadScene("City2");
+        SceneManager.LoadScene("Level7", LoadSceneMode.Additive);
+    }
+    public void Restart8()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Restart");
+        SceneManager.LoadScene("City2");
+        SceneManager.LoadScene("Level8", LoadSceneMode.Additive);
+    }
+
+}
